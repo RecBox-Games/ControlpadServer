0fec8501c586d2dcdc06eada4572096639da2b9a
Weston Kelliher <westonkelliher@gmail.com>
2023-06-23 15:09:05 -0700

    Handle websocket connection case where the handshake is interupted and split up new_connections() (which handles new websocket connections) into 3 functions

--- src/saws.rs
+++ src/saws.rs
@@ -1,7 +1,8 @@
 // Simple A** Web Sockets
 use std::net::{TcpStream, TcpListener};
-use tungstenite::{WebSocket, accept};
 use tungstenite;
+use tungstenite::{WebSocket, accept, ServerHandshake};
+use tungstenite::handshake::{MidHandshake, server::NoCallback, HandshakeError};
 use crate::util::Result;
 
 pub enum Msg {
@@ -18,10 +19,15 @@ pub struct Conn {
 }
 
 impl Conn {
-    pub fn new(mut ws: WebSocket<TcpStream>, addr: String) -> Result<Self> {
-	//ws.get_mut().set_nonblocking(true)?;
-        
-        // TODO: increase timeout
+    pub fn new(ws: WebSocket<TcpStream>) -> Result<Self> {
+        let addr = match ws.get_ref().peer_addr() {
+            Ok(sock_addr) => sock_addr.to_string(),
+            Err(e) => {
+                println!("failure getting websocket address: {}", e);
+                "0.0.0.0".to_string()
+            }
+        };
+        // TODO: potentially increase timeout
 	Ok(Conn {
 	    websocket: ws,
 	    addr: addr,
@@ -97,7 +103,11 @@ impl Conn {
 
 pub struct Server {
     server: TcpListener,
+    handshake_continuation: Option<MidHandshake<ServerHandshake<TcpStream, NoCallback>>>,
 }
+
+type HandshakeResult = std::result::Result<WebSocket<TcpStream>,
+                              HandshakeError<ServerHandshake<TcpStream, NoCallback>>>;
 
 impl Server {
     pub fn new(port: &str) -> Result<Self> {
@@ -105,41 +115,66 @@ impl Server {
 	server.set_nonblocking(true)?;
 	Ok(Server {
 	    server: server,
+            handshake_continuation: None,
 	})
     }
 
-    pub fn new_connections(&self) -> Vec<Conn> {
+    fn websocket_from_handshake_result(&mut self, result: HandshakeResult) -> Option<WebSocket<TcpStream>> {
+        match result {
+	    Ok(websocket) => {
+                Some(websocket)
+	    }
+	    Err(HandshakeError::Interrupted(mid_handshake)) => {
+                self.handshake_continuation = Some(mid_handshake);
+                None
+	    }
+	    Err(e) => {
+		println!("Warning: Error during websocket handshake: {}", e);
+                None
+	    }
+        }
+    }
+
+    fn get_next_websocket(&mut self) -> Option<WebSocket<TcpStream>> {
+        // in-progress handshaking connection
+        if let Some(mid_handshake) = self.handshake_continuation.take() {
+            let result = mid_handshake.handshake();
+            return self.websocket_from_handshake_result(result);
+        } 
+        // brand new connection
+	match self.server.accept() {
+	    Ok((stream, _)) => {
+                if let Err(e) = stream.set_nonblocking(true) {
+                    println!("Failed to set stream to nonblocking before accept(): {}", e);
+                }
+                let result = accept(stream);
+                self.websocket_from_handshake_result(result)
+	    }
+	    Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                None
+	    }
+            Err(e) => {
+		println!("Warning: Unexpected error when trying to accept a connection: {}", e);
+                None
+            }                
+	}
+    }
+
+    pub fn new_connections(&mut self) -> Vec<Conn> {
 	let mut conns: Vec<Conn> = vec![];
 	loop {
-	    match self.server.accept() {
-		Ok((stream, addr)) => {
-                    if let Err(e) = stream.set_nonblocking(true) {
-                        println!("Failed to set stream to nonblocking before accept(): {}", e);
-                    }
-		    match accept(stream) {
-			Ok(websocket) => {
-			    match Conn::new(websocket, addr.to_string()) {
-				Ok(conn) => {
- 				    conns.push(conn);
-				}
-				Err(e) => {
-				    println!("Warning: Error when trying to create a conn: {}", e);
-				}
-			    }
-			}
-			Err(e) => {
-			    println!("Warning: Error when trying to validate a connection (accept stream): {}", e);
-			}
+            if let Some(websocket) = self.get_next_websocket() {
+                match Conn::new(websocket) {
+		    Ok(conn) => {
+ 		        conns.push(conn);
 		    }
-		}
-		Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-		    break;
-		}
-		Err(e) => {
-		    println!("Warning: Unexpected error when trying to accept a connection: {}", e);
-		    break;
-		}
-	    }
+		    Err(e) => {
+		        println!("Warning: Error when trying to create a conn: {}", e);
+		    }
+	        }
+            } else {
+                break;
+            }
 	}
 	return conns;
     }
