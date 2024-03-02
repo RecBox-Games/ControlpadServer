b6a95062159f22a66e2a910974cbeaf029a7a866
Safwaan Menk <41082275+safwaan-menk@users.noreply.github.com>
2024-03-02 14:15:21 -0800

    Rpc in qr (#4)
    * add getqr message

--- src/server.rs
+++ src/server.rs
@@ -63,10 +63,14 @@ fn write_msgs_from_client(id: String, msgs: Vec<String>) -> Result<()> {
     Ok(())
 }
 
-fn write_rpc_from_client(data: &Vec<u8>) -> Result<()> {   
+fn write_rpc_from_client(data: &Vec<u8>) -> Result<()> {
     let ipc_name = "rpc_in";
     if *data == vec![0x99, 0x99] {
-        ipc::write(ipc_name, "quit")?;        
+        let s = "quit".to_string() + str::from_utf8(&[0])?;        
+        ipc::write(ipc_name, &s)?;        
+    } else if *data == vec![0x98, 0x98] {
+        let s = "getqr".to_string() + str::from_utf8(&[0])?;        
+        ipc::write(ipc_name, &s)?;
     } else {
         println!("Warning: received invalid rpc message: {:?}", data);
     }
