5bb5103baa9302d23b71211db4d62084d4d3fa67
Weston <westonkelliher@gmail.com>
2024-06-18 16:21:59 -0700

    first commit for this branch

--- src/ipc_test.rs
+++ src/ipc_test.rs
@@ -1,21 +1,28 @@
 mod ipc;
+mod systemlock;
+use std::time::Instant;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("dirty: {}", ipc::has_new("foo")?);
-    println!("read: {}", ipc::read("foo")?);
-    println!("dirty: {}", ipc::has_new("foo")?);
+    let start = Instant::now();
+    //
+    ipc::has_new("foo")?;
+    ipc::read("foo")?;
+    ipc::has_new("foo")?;
     let s1 = "Brundwich champions\n";
-    println!("writing: {}", s1);
     ipc::write("foo", s1)?;
-    println!("dirty: {}", ipc::has_new("foo")?);
+    ipc::has_new("foo")?;
     let s2 = "Champion Schmampion\n";
-    println!("writing: {}", s2);
     ipc::write("foo", s2)?;
-    println!("dirty: {}", ipc::has_new("foo")?);
-    println!("consume: {}", ipc::consume("foo")?);
-    println!("dirty: {}", ipc::has_new("foo")?);
+    ipc::write("baz", s2)?;
+    ipc::has_new("foo")?;
+    ipc::consume("foo")?;
+    ipc::consume("baz")?;
+    ipc::has_new("foo")?;
+    ipc::has_new("baz")?;
     let s3 = "I'm written last with no newline.";
-    println!("writing: {}", s3);
     ipc::write("foo", s3)?;
+    //
+    let elapsed = start.elapsed();
+    println!("Elapsed time: {} microseconds", elapsed.as_micros());
     Ok(())
 }
